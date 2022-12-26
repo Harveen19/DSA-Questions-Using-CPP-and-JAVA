c21b2df59e7d164383ad540aa24a5e3fd8b39b47
Harveen19 <harveenkaur1912@gmail.com>
2022-12-26 19:00:18 +0530

    Add all my files

---
+++ LINKED LIST/Deletion in LL.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+void insertAtTail(node* &head, int val){
+
+    node* n= new node(val);
+    if(head==NULL){
+        head=n;
+        return;
+    }
+    node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+void display(node* head){
+    node* temp=head;
+    while (temp!=NULL)
+    {
+        /* code */
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+void deleteAtHead(node* &head){
+    node* todelete=head;
+    head=head->next;
+
+    delete todelete;
+}
+ 
+void deletion(node* &head, int val){
+    node* temp=head;
+    while(temp->next->data != val){
+        temp=temp->next;
+    }
+    node* todelete=temp->next;
+    temp->next=temp->next->next;
+
+    delete todelete;
+}
+
+int main(){
+
+    node* head=NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    insertAtTail(head,4);
+    display(head);
+
+    deleteAtHead(head);
+    //deletion(head,3);
+    display(head);
+
+}
