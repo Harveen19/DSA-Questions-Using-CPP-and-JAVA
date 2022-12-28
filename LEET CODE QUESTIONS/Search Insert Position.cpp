0af3ba0465b093ab0ddcb7dbe5540fc26efbad0d
Harveen19 <harveenkaur1912@gmail.com>
2022-12-28 20:01:04 +0530

    Add all my files

--- LEET CODE QUESTIONS/Search Insert Position.cpp
+++ LEET CODE QUESTIONS/Search Insert Position.cpp
@@ -22,20 +22,21 @@
  
 
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int s=0, e=nums.length-1;
-        while(s<=e){
-            int mid= (s+e)/2;
-            if(nums[mid]==target){
-                return mid;
+    public:
+        int searchInsert(vector<int>& nums, int target) {
+            int s=0, e=nums.size()-1;
+            while(s<=e){
+                int mid= (s+e)/2;
+                if(nums[mid]==target){
+                    return mid;
+                }
+                else if(target>nums[mid]){
+                    s=mid+1;
+                }
+                else{
+                    e=mid-1;
+                }
             }
-            else if(target>nums[mid]){
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
+            return e+1;
         }
-        return e+1;
-    }
-}
+};
