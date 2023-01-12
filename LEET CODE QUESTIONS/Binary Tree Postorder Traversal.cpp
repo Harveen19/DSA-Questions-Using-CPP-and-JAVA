65101830a86610cf597262f9d6b48884e1be40aa
Harveen Kaur <78599929+Harveen19@users.noreply.github.com>
2023-01-12 19:36:49 +0530

    Add all my files

---
+++ LEET CODE QUESTIONS/Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,50 @@
+/* 145. Binary Tree Postorder Traversal
+Easy
+5.5K
+163
+Companies
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root==nullptr){
+            return {};
+        }
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        ans.push_back(root->val);
+
+        return ans;
+    }
+};
