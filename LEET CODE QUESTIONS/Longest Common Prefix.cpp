995bb08270a7c88af384599c5b2591df7f091853
Harveen Kaur <78599929+Harveen19@users.noreply.github.com>
2023-01-16 21:09:43 +0530

    Add all my files

---
+++ LEET CODE QUESTIONS/Longest Common Prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string s=strs[0];
+        for(int i=1;i<strs.size();i++){
+            int k=0, j=0;
+            while(k<s.size() || j<strs[i].size()){
+                if(s[k]==strs[i][j]){
+                    j++;
+                    k++;
+                }
+                else{
+                    s=strs[i].substr(0,j);
+                    break;
+                }
+            }
+        }
+        return s;
+    }
+};
