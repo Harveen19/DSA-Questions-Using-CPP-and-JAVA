b505225b9b8fbc4769a44147d012cef6c6bcfde4
Harveen Kaur <78599929+Harveen19@users.noreply.github.com>
2023-04-03 20:32:05 +0530

    Add all my files

---
+++ LEET CODE QUESTIONS/Boats to Save People.java
@@ -0,0 +1,43 @@
+/* 881. Boats to Save People
+Medium
+4.8K
+114
+Companies
+You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+Return the minimum number of boats to carry every given person.
+
+ 
+
+Example 1:
+
+Input: people = [1,2], limit = 3
+Output: 1
+Explanation: 1 boat (1, 2)
+Example 2:
+
+Input: people = [3,2,2,1], limit = 3
+Output: 3
+Explanation: 3 boats (1, 2), (2) and (3)
+Example 3:
+
+Input: people = [3,5,3,4], limit = 5
+Output: 4
+Explanation: 4 boats (3), (3), (4), (5)
+*/
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int ans = 0;
+        Arrays.sort(people);
+
+        int i=0;
+        for(int j=people.length-1; i<=j; ans++){
+            int remaining = limit - people[j--];
+            if(people[i] <= remaining){
+                i++;
+            }
+        }
+        return ans;
+    }
+}
