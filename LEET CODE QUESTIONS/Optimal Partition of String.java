6b799f34dfde5acc1725df1e7cb061994c53c749
Harveen Kaur <78599929+Harveen19@users.noreply.github.com>
2023-04-12 21:40:25 +0530

    Add all my files

---
+++ LEET CODE QUESTIONS/Optimal Partition of String.java
@@ -0,0 +1,43 @@
+/* 2405. Optimal Partition of String
+Medium
+2K
+75
+Companies
+Given a string s, partition the string into one or more substrings such that the characters in each substring are unique. That is, no letter appears in a single substring more than once.
+
+Return the minimum number of substrings in such a partition.
+
+Note that each character should belong to exactly one substring in a partition.
+
+ 
+
+Example 1:
+
+Input: s = "abacaba"
+Output: 4
+Explanation:
+Two possible partitions are ("a","ba","cab","a") and ("ab","a","ca","ba").
+It can be shown that 4 is the minimum number of substrings needed.
+Example 2:
+
+Input: s = "ssssss"
+Output: 6
+Explanation:
+The only valid partition is ("s","s","s","s","s","s").
+*/
+
+class Solution {
+    public int partitionString(String s) {
+        int[] last_pos = new int[26];
+        int ans = 0;
+        int last_end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (last_pos[s.charAt(i) - 'a'] >= last_end) {
+                ans++;
+                last_end = i + 1;
+            }
+            last_pos[s.charAt(i) - 'a'] = i + 1;
+        }
+        return ans;
+    }
+}
