f0e3f83dc296efa558c90385c12f321155d73a3b
Harveen Kaur <78599929+Harveen19@users.noreply.github.com>
2023-04-20 21:24:14 +0530

    Add all my files

--- JAVA/Reverse a String.java
+++ JAVA/Reverse a String.java
@@ -2,6 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+    // Using Recursion
+    public static void revString(String str, int idx){
+        if(idx == 0){
+            System.out.println(str.charAt(idx));
+            return;
+        }
+        
+        System.out.print(str.charAt(idx));
+        revString(str, idx-1);
+        
+    }
+    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		revString(str, str.length()-1);
+		
+		
+	}
+}
+
+    // ITERATIVE METHOD
     public static void main(String[] args) {
         String nstr= "", str= " ReversE ";
         char ch;
@@ -16,3 +39,4 @@ public class Solution {
         System.out.println("Reversed word: " + nstr);
     }
 }
+
