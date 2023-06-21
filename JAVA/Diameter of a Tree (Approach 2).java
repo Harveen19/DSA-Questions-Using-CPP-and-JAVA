60318f11f967b8159dd7e6f6f6a8a1510c2dff2d
Harveen Kaur <78599929+Harveen19@users.noreply.github.com>
2023-06-21 18:42:50 +0530

    Add all my files

--- JAVA/Diameter of a Tree (Approach 2).java
+++ JAVA/Diameter of a Tree (Approach 2).java
@@ -61,7 +61,7 @@ public class Main
     }
     
     public static void main(String[] args) {
-		Node root = new Node(1);
+	Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
@@ -69,8 +69,8 @@ public class Main
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 		
-		System.out.println(diameter(root).diam);
-	}
+	System.out.println(diameter(root).diam);
+    }
 }
 
 // Time Complexity -> O(n) 
